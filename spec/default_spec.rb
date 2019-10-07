e769df419ac98e33e50f7876da8b7c4092b8c2a4
f5030161 <rudi.kramer@fnb.co.za>
2019-10-07 11:53:35 +0200

    Removed deprecations from unit tests

--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -1,31 +1,63 @@
 require 'spec_helper'
 
 describe 'iptables-ng::default' do
-  let(:chef_run) do
-    ChefSpec::Runner.new do |node|
-      node.automatic.merge!(JSON.parse(File.read('test/nodes/test.json')))
-    end.converge(described_recipe)
+  context 'When all attributes are default, on CentOS 7' do
+    let(:chef_run) do
+      ChefSpec::SoloRunner.new(platform: 'centos', version: '7') do |node|
+        node.automatic.merge!(JSON.parse(File.read('test/nodes/test.json')))
+      end.converge(described_recipe)
+    end
+
+    it 'should include iptables-ng::install' do
+      expect(chef_run).to include_recipe('iptables-ng::install')
+    end
+
+    it 'should apply rules from node definition' do
+      expect(chef_run).to create_iptables_ng_rule('test1-filter-INPUT-attribute-rule')
+        .with(
+          chain: 'INPUT',
+          table: 'filter',
+          rule: '--protocol tcp --dport 22 --match state --state NEW --jump ACCEPT',
+          ip_version: [4, 6],
+        )
+
+      expect(chef_run).to create_iptables_ng_rule('test2-filter-INPUT-attribute-rule')
+        .with(
+          chain: 'INPUT',
+          table: 'filter',
+          rule: '--protocol tcp --dport 80 --match state --state NEW --jump DROP',
+          ip_version: 4,
+        )
+    end
   end
 
-  it 'should include iptables-ng::install' do
-    expect(chef_run).to include_recipe('iptables-ng::install')
-  end
+  context 'When all attributes are default, on Debian 10' do
+    let(:chef_run) do
+      ChefSpec::SoloRunner.new(platform: 'debian', version: '10') do |node|
+        node.automatic.merge!(JSON.parse(File.read('test/nodes/test.json')))
+      end.converge(described_recipe)
+    end
 
-  it 'should apply rules from node definition' do
-    expect(chef_run).to create_iptables_ng_rule('test1-filter-INPUT-attribute-rule')
-      .with(
-        chain: 'INPUT',
-        table: 'filter',
-        rule: '--protocol tcp --dport 22 --match state --state NEW --jump ACCEPT',
-        ip_version: [4, 6]
-      )
+    it 'should include iptables-ng::install' do
+      expect(chef_run).to include_recipe('iptables-ng::install')
+    end
 
-    expect(chef_run).to create_iptables_ng_rule('test2-filter-INPUT-attribute-rule')
-      .with(
-        chain: 'INPUT',
-        table: 'filter',
-        rule: '--protocol tcp --dport 80 --match state --state NEW --jump DROP',
-        ip_version: 4
-      )
+    it 'should apply rules from node definition' do
+      expect(chef_run).to create_iptables_ng_rule('test1-filter-INPUT-attribute-rule')
+        .with(
+          chain: 'INPUT',
+          table: 'filter',
+          rule: '--protocol tcp --dport 22 --match state --state NEW --jump ACCEPT',
+          ip_version: [4, 6],
+        )
+
+      expect(chef_run).to create_iptables_ng_rule('test2-filter-INPUT-attribute-rule')
+        .with(
+          chain: 'INPUT',
+          table: 'filter',
+          rule: '--protocol tcp --dport 80 --match state --state NEW --jump DROP',
+          ip_version: 4,
+        )
+    end
   end
 end
--- spec/install_spec.rb
+++ spec/install_spec.rb
@@ -1,48 +1,83 @@
 require 'spec_helper'
 
 describe 'iptables-ng::install' do
-  describe 'debian' do
-    let(:chef_run) do
-      ChefSpec::Runner.new do |node|
-        node.automatic['platform_family'] = 'debian'
-      end.converge(described_recipe)
-    end
+  context 'When all attributes are default, on Debian 10' do
+    # for a complete list of available platforms and versions see:
+    # https://github.com/chefspec/fauxhai/blob/master/PLATFORMS.md
+    platform 'debian', '10'
 
     it 'should remove ufw' do
       expect(chef_run).to remove_package('iptables-ng::install - ufw package')
     end
   end
 
-  let(:chef_run) do
-    ChefSpec::Runner.new do |node|
-      node.automatic.merge!(JSON.parse(File.read('test/nodes/test.json')))
-    end.converge(described_recipe)
+  context 'When all attributes are default, on CentOS 7' do
+    let(:chef_run) do
+      ChefSpec::SoloRunner.new(platform: 'centos', version: '7') do |node|
+        node.automatic.merge!(JSON.parse(File.read('test/nodes/test.json')))
+      end.converge(described_recipe)
+    end
+
+    it 'should include iptables-ng::manage' do
+      expect(chef_run).to include_recipe('iptables-ng::manage')
+    end
+
+    it 'should create directory for chains' do
+      expect(chef_run).to create_directory('/etc/iptables.d/filter')
+      expect(chef_run).to create_directory('/etc/iptables.d/nat')
+      expect(chef_run).to create_directory('/etc/iptables.d/mangle')
+      expect(chef_run).to create_directory('/etc/iptables.d/raw')
+    end
+
+    it 'should apply default policies' do
+      expect(chef_run).to create_if_missing_iptables_ng_chain('default-policy-filter-INPUT')
+      expect(chef_run).to create_if_missing_iptables_ng_chain('default-policy-filter-OUTPUT')
+      expect(chef_run).to create_if_missing_iptables_ng_chain('default-policy-filter-FORWARD')
+      expect(chef_run).to create_if_missing_iptables_ng_chain('default-policy-nat-OUTPUT')
+      expect(chef_run).to create_if_missing_iptables_ng_chain('default-policy-nat-PREROUTING')
+      expect(chef_run).to create_if_missing_iptables_ng_chain('default-policy-nat-POSTROUTING')
+      expect(chef_run).to create_if_missing_iptables_ng_chain('default-policy-mangle-INPUT')
+      expect(chef_run).to create_if_missing_iptables_ng_chain('default-policy-mangle-OUTPUT')
+      expect(chef_run).to create_if_missing_iptables_ng_chain('default-policy-mangle-FORWARD')
+      expect(chef_run).to create_if_missing_iptables_ng_chain('default-policy-mangle-PREROUTING')
+      expect(chef_run).to create_if_missing_iptables_ng_chain('default-policy-mangle-POSTROUTING')
+      expect(chef_run).to create_if_missing_iptables_ng_chain('default-policy-raw-OUTPUT')
+      expect(chef_run).to create_if_missing_iptables_ng_chain('default-policy-raw-PREROUTING')
+    end
   end
 
-  it 'should include iptables-ng::manage' do
-    expect(chef_run).to include_recipe('iptables-ng::manage')
-  end
+  context 'When all attributes are default, on Debian 10' do
+    let(:chef_run) do
+      ChefSpec::SoloRunner.new(platform: 'debian', version: '10') do |node|
+        node.automatic.merge!(JSON.parse(File.read('test/nodes/test.json')))
+      end.converge(described_recipe)
+    end
 
-  it 'should create directory for chains' do
-    expect(chef_run).to create_directory('/etc/iptables.d/filter')
-    expect(chef_run).to create_directory('/etc/iptables.d/nat')
-    expect(chef_run).to create_directory('/etc/iptables.d/mangle')
-    expect(chef_run).to create_directory('/etc/iptables.d/raw')
-  end
+    it 'should include iptables-ng::manage' do
+      expect(chef_run).to include_recipe('iptables-ng::manage')
+    end
 
-  it 'should apply default policies' do
-    expect(chef_run).to create_if_missing_iptables_ng_chain('default-policy-filter-INPUT')
-    expect(chef_run).to create_if_missing_iptables_ng_chain('default-policy-filter-OUTPUT')
-    expect(chef_run).to create_if_missing_iptables_ng_chain('default-policy-filter-FORWARD')
-    expect(chef_run).to create_if_missing_iptables_ng_chain('default-policy-nat-OUTPUT')
-    expect(chef_run).to create_if_missing_iptables_ng_chain('default-policy-nat-PREROUTING')
-    expect(chef_run).to create_if_missing_iptables_ng_chain('default-policy-nat-POSTROUTING')
-    expect(chef_run).to create_if_missing_iptables_ng_chain('default-policy-mangle-INPUT')
-    expect(chef_run).to create_if_missing_iptables_ng_chain('default-policy-mangle-OUTPUT')
-    expect(chef_run).to create_if_missing_iptables_ng_chain('default-policy-mangle-FORWARD')
-    expect(chef_run).to create_if_missing_iptables_ng_chain('default-policy-mangle-PREROUTING')
-    expect(chef_run).to create_if_missing_iptables_ng_chain('default-policy-mangle-POSTROUTING')
-    expect(chef_run).to create_if_missing_iptables_ng_chain('default-policy-raw-OUTPUT')
-    expect(chef_run).to create_if_missing_iptables_ng_chain('default-policy-raw-PREROUTING')
+    it 'should create directory for chains' do
+      expect(chef_run).to create_directory('/etc/iptables.d/filter')
+      expect(chef_run).to create_directory('/etc/iptables.d/nat')
+      expect(chef_run).to create_directory('/etc/iptables.d/mangle')
+      expect(chef_run).to create_directory('/etc/iptables.d/raw')
+    end
+
+    it 'should apply default policies' do
+      expect(chef_run).to create_if_missing_iptables_ng_chain('default-policy-filter-INPUT')
+      expect(chef_run).to create_if_missing_iptables_ng_chain('default-policy-filter-OUTPUT')
+      expect(chef_run).to create_if_missing_iptables_ng_chain('default-policy-filter-FORWARD')
+      expect(chef_run).to create_if_missing_iptables_ng_chain('default-policy-nat-OUTPUT')
+      expect(chef_run).to create_if_missing_iptables_ng_chain('default-policy-nat-PREROUTING')
+      expect(chef_run).to create_if_missing_iptables_ng_chain('default-policy-nat-POSTROUTING')
+      expect(chef_run).to create_if_missing_iptables_ng_chain('default-policy-mangle-INPUT')
+      expect(chef_run).to create_if_missing_iptables_ng_chain('default-policy-mangle-OUTPUT')
+      expect(chef_run).to create_if_missing_iptables_ng_chain('default-policy-mangle-FORWARD')
+      expect(chef_run).to create_if_missing_iptables_ng_chain('default-policy-mangle-PREROUTING')
+      expect(chef_run).to create_if_missing_iptables_ng_chain('default-policy-mangle-POSTROUTING')
+      expect(chef_run).to create_if_missing_iptables_ng_chain('default-policy-raw-OUTPUT')
+      expect(chef_run).to create_if_missing_iptables_ng_chain('default-policy-raw-PREROUTING')
+    end
   end
 end
